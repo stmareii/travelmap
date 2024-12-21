264ccb5b42fed374ede7cb0dfec4ccdb4e8b61d8
kutikq <kutenkov5yandex.ru@mail.ru>
2024-12-21 07:13:39 +0300

    -

--- leafnet.js
+++ leafnet.js
@@ -7,16 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: "&copy; OpenStreetMap contributors",
     }).addTo(map);
-
-    // Define locations with coordinates and descriptions
-    const locations = [
-        { lat: 55.7558, lon: 37.6173, name: "Место 1" },
-        { lat: 55.5657, lon: 37.6515, name: "Место 2" },
-        { lat: 55.4657, lon: 37.5515, name: "Место 3" },
-        { lat: 55.6657, lon: 37.7515, name: "Место 4" },
-        { lat: 55.8657, lon: 37.9515, name: "Место 5" },
-    ];
-
+    
     // Add markers to the map
     locations.forEach(({ lat, lon, name }) => {
         const marker = L.marker([lat, lon]).addTo(map);
